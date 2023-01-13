14fdfb81fb4fb9566f81f3be053cc105fb8191d8
Charizard17 <3723esad@gmail.com>
2023-01-13 13:22:59 +0100

    profile page and rightbar component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./home.css";
-import Topbar from "../components/topbar/Topbar";
-import Sidebar from "../components/sidebar/Sidebar";
-import Feed from "../components/feed/Feed";
-import Rightbar from "../components/rightbar/Rightbar";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 
 export default function Home() {
   return (
